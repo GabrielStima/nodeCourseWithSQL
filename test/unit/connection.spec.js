526caaf28b4acb0174fb2839a5ebb28b6d78ba32
Gabriel <gabrielstimamiglio@gmail.com>
2020-04-06 22:44:22 -0300

    Trying make a test for db connection

--- test/unit/connection.spec.js
+++ test/unit/connection.spec.js
@@ -1,17 +1,23 @@
 const Sequelize = require("sequelize");
 const { connection } = require("../../src/database/connection");
+const sequelize = connection(Sequelize);
 
-describe("Should return true", () => {
-  let result;
-  const sequelize = connection(Sequelize);
+// function teste (callback) {
+//   sequelize.authenticate().then(() => {
+//     callback(true);
+//   }).catch(() =>{
+//     callback(false);
+//   })
+// }
 
-  console.log(
-    "uma hora vai",
-    sequelize
-      .authenticate()
-      .then(() => console.log("deu certo"))
-      .catch(() => console.log("deu errado"))
-  );
+// it("Should return true", async (
+  
+// ) => {
+//   return 
+// });
 
-  expect(result).toBeTruthy();
+it("Should return true", async () => {
+  expect.assertions(1);
+  const data = sequelize.authenticate().then(() => true);
+  expect(data).toBeTruthy();
 });
